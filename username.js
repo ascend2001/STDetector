2cbb3c09db63426c547a57ad89a4e16d990de576
Avnish Sengupta <avnishsengupta1@g.ucla.edu>
2021-03-25 01:36:12 -0700

    final commit

---
+++ username.js
@@ -0,0 +1,54 @@
+// import React from 'react'
+// import { TextInput } from 'react-native';
+// import styled from 'styled-components'
+// import {Container} from './App'
+
+// const Credentials_Container= styled.View`
+// position: absolute;
+// width: 341px;
+// height: 348px;
+// left: 18px;
+// top: 173px;
+
+// background: #000000;
+// border-radius: 30px;
+// `;
+
+// const Credentials_Title=styled.Text `
+// position: absolute;
+// width: 113px;
+// height: 32px;
+// left: 29px;
+// top: 223px;
+
+// font-family: PT Sans;
+// font-style: normal;
+// font-weight: normal;
+// font-size: 25px;
+// line-height: 32px;
+// text-align: center;
+
+// color: #03DAC5;
+// `;
+
+// export default function LoginPage(){
+//     return(
+//         <Container>
+//             <Credentials_Container>
+//                 <Credentials_Title>
+//                     Username: 
+//                 </Credentials_Title>
+//                 <TextInput>
+//                     True
+//                 </TextInput>
+//                 <Credentials_Title>
+//                     Password:
+//                 </Credentials_Title>
+//                 <TextInput>
+//                     True
+//                 </TextInput>
+//             </Credentials_Container>
+
+//         </Container>
+//     );
+// }
